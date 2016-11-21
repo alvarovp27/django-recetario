f41f35fa34fe0cd8a61fc3f931694649aa648378
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-21 20:24:29 +0100

    feat: added a new model (Receta), and project configured for supporting image upload and visualization

--- recetario/urls.py
+++ recetario/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -23,4 +23,6 @@ urlpatterns = patterns('',
     url(r'^$', 'principal.views.lista_bebidas'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+        {'document_root':settings.MEDIA_ROOT,})
 )
