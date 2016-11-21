99e9e0f1717d2cdd2ba8a96da32f87c28d0afd9d
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-21 21:41:36 +0100

    added list of Recetas and details of Receta views and templates

--- principal/views.py
+++ principal/views.py
@@ -1,7 +1,9 @@
+#encoding:utf-8
 # Create your views here.
 from principal.models import Bebida, Receta, User
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+from django.template import RequestContext
 
 def lista_bebidas(request):
     bebidas = Bebida.objects.all()
@@ -19,3 +21,15 @@ def usuarios(request):
     users = User.objects.all()
     recetas = Receta.objects.all()
     return render_to_response('usuarios.html', {'usuarios': users, 'recetas': recetas})
+
+def lista_recetas(request):
+    recetas = Receta.objects.all()
+    #context_instance -> we've to use this parameter for accessing to the image
+    #return render_to_response('recetas.html', {'recetas': recetas}, context_instance=RequestContext(request))
+    #RequestContext did not work because it did not give me the right uri to obtain the path of "carga" folder
+    #so i had to do a work around
+    return render_to_response('recetas.html', {'recetas': recetas})
+
+def detalle_receta(request, id_receta):
+    dato = get_object_or_404(Receta, pk=id_receta)
+    return render_to_response('receta.html', {'receta': dato})
--- recetario/urls.py
+++ recetario/urls.py
@@ -22,6 +22,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'principal.views.inicio'),
     url(r'^usuarios/$', 'principal.views.usuarios'),
+    url(r'^recetas/$', 'principal.views.lista_recetas'),
+    url(r'^receta/(?P<id_receta>\d+)$', 'principal.views.detalle_receta'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',
