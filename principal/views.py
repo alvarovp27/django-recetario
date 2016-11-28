9b6be013bb41b81b3a3495da7ed75094d4011d5b
Alvaro Valencia <avalenciaparra@gmail.com>
2016-11-28 20:21:27 +0100

    feat: users management

--- principal/views.py
+++ principal/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from httplib import HTTPResponse
 
 
 def lista_bebidas(request):
@@ -86,3 +87,33 @@ def nuevo_usuario(request):
         formulario = UserCreationForm()
     return render_to_response('nuevousuario.html', {'formulario': formulario}, context_instance=RequestContext(request))
 
+def ingresar(request):
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('/privado')
+    if request.method=='POST':
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST['username']
+            clave = request.POST['password']
+            acceso = authenticate(username = usuario, password=clave)
+            if acceso is not None:
+                if acceso.is_active:
+                    login(request,acceso)
+                    return HttpResponseRedirect('/privado')
+                else:
+                    return render_to_response('noactivo.html', context_instance=RequestContext(request))
+            else:
+                return render_to_response('nousuario.html', context_instance=RequestContext(request))
+    else:
+        formulario = AuthenticationForm()
+    return render_to_response('ingresar.html', {'formulario': formulario}, context_instance=RequestContext(request))
+
+@login_required(login_url='/ingresar')
+def privado(request):
+    usuario = request.user
+    return render_to_response('privado.html', {'usuario': usuario}, context_instance = RequestContext(request))
+
+@login_required(login_url='/ingresar')
+def cerrar(request):
+    logout(request)
+    return HttpResponseRedirect('/')
